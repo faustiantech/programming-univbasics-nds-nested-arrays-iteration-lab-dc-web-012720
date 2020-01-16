02df12ef8ccf0d48b07c46047e5158a689c6bb15
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-16 14:38:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,7 +12,7 @@ def join_ingredients(src)
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    listoffoods.push(src[row_index][element_index])
+    listoffoods.push("I love #{src[row_index][element_index]}")
     element_index += 1
   end
   row_index += 1
